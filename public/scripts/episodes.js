571bde178cf3f92f9d34805390ccdec04b6f1eaa
Yeetov <75633586+Santabo@users.noreply.github.com>
2025-06-18 23:24:52 +0100

    Added Season 5

--- public/scripts/episodes.js
+++ public/scripts/episodes.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const seasons = ['season1', 'season2', 'season3', 'season4']; // Add more as needed
+  const seasons = ['season1', 'season2', 'season3', 'season4', 'season5']; // Add more as needed
   const container = document.getElementById('episode-list');
   const modal = document.getElementById('video-modal');
   const iframe = document.getElementById('modal-video');
